333b5d8e7a87d2e47842dd5509dbe274de0ac095
sirisian <sirisian@sirisian.com>
2025-03-06 13:46:01 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+import Fastify from 'fastify';
+
+const fastify = Fastify({});
+
+fastify.get('/fooupdate', {}, async (request, reply) => {});
+fastify.get('/foodelete', {}, async (request, reply) => {});
+fastify.get('/mymethod', {}, async (request, reply) => {});
+fastify.get('/mymethod42', {}, async (request, reply) => {});
+
+console.log('commonPrefix: true');
+console.log(fastify.printRoutes({ commonPrefix: true }));
+console.log('commonPrefix: false');
+console.log(fastify.printRoutes({ commonPrefix: false }));
